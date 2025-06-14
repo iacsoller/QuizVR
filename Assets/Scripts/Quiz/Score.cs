5297ca55d6933ae9816b31c1b840d1b6ce77aee8
iacsoller <168876397+iacsoller@users.noreply.github.com>
2025-06-14 14:17:17 +0800

    Added apples, basket, score system, UI

---
+++ Assets/Scripts/Quiz/Score.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Apple") )
+        {
+            ScoreSystem.score++;
+        }
+
+        
+    }
+}
